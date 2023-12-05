7188e668247a5109d9dabb69542708e4cca0c69c
Daniel <47531721+AwuyaDaniel@users.noreply.github.com>
2023-12-05 09:59:44 +0100

    first commit

--- Chat/consumers.py
+++ Chat/consumers.py
@@ -6,25 +6,41 @@ import json
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        # Code to handle WebSocket connection
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = f'chat_{self.room_name}'
+
+        # Join room group
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+
         await self.accept()
-        pass
 
     async def disconnect(self, close_code):
-        # Code to handle WebSocket disconnection
-
-        pass
+        # Leave room group
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
 
     async def receive(self, text_data):
-        # Code to handle receiving a message from WebSocket
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
 
+        # Send message to room group
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message
+            }
+        )
+
+    async def chat_message(self, event):
+        message = event['message']
+
+        # Send message to WebSocket
         await self.send(text_data=json.dumps({
             'message': message
         }))
-        pass
-
-    async def send_message(self, event):
-        # Code to send a message to the WebSocket
-        pass
